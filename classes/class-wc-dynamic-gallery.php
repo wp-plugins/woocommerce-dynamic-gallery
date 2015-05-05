f7b239f91f14cc7b3e18a045511ee9323679bf47
a3rev <a3rev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-05 09:22:02 +0000

    Update to new version 1.3.5
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-dynamic-gallery/trunk@1153584 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/class-wc-dynamic-gallery.php
+++ classes/class-wc-dynamic-gallery.php
@@ -210,7 +210,7 @@ class WC_Gallery_Display_Class
                                    $alt = get_post_meta($item_thumb->ID, '_wp_attachment_image_alt', true);
 								   $img_description = $item_thumb->post_excerpt;
 
-                                    echo '<li class="'.$li_class.'"><a alt="'.$alt.'" class="gallery_product_'.$post->ID.' gallery_product_'.$post->ID.'_'.$idx.'" title="'. esc_attr( $img_description ) .'" rel="gallery_product_'.$post->ID.'" href="'.$image_lager_default_url.'"><div><img idx="'.$idx.'" style="width:'.$thumb_width.'px !important;height:'.$thumb_height.'px !important" src="'.$image_thumb_default_url.'" alt="'. esc_attr( $img_description ) .'" class="image'.$i.'" width="'.$thumb_width.'" height="'.$thumb_height.'"></div></a></li>';
+                                    echo '<li class="'.$li_class.'"><a alt="'. esc_attr( $alt ) .'" class="gallery_product_'.$post->ID.' gallery_product_'.$post->ID.'_'.$idx.'" title="'. esc_attr( $img_description ) .'" rel="gallery_product_'.$post->ID.'" href="'.$image_lager_default_url.'"><div><img idx="'.$idx.'" style="width:'.$thumb_width.'px !important;height:'.$thumb_height.'px !important" src="'.$image_thumb_default_url.'" alt="'. esc_attr( $alt ) .'" data-caption="'. esc_attr( $img_description ) .'" class="image'.$i.'" width="'.$thumb_width.'" height="'.$thumb_height.'"></div></a></li>';
                                     $img_description = esc_js( $img_description );
                                     if($img_description != ''){
 										$script_fancybox .= $common.'{href:"'.$image_lager_default_url.'",title:"'.$img_description.'"}';
